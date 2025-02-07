bd54b2aea3d949ace887528bce32f0a15d400faf
SakshiLadole06 <ladolesakshi@gmail.com>
2025-02-07 12:11:40 +0530

    dimens commited

--- app/src/main/java/com/example/taskandroid/MainActivity.kt
+++ app/src/main/java/com/example/taskandroid/MainActivity.kt
@@ -7,11 +7,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.taskandroid.dimenResponse.dimen_responsive
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+        //Button for the clicking 4. Dimens for responsive design
+        val demanResponseBtn:Button =findViewById(R.id.btn4)
+        demanResponseBtn.setOnClickListener {
+            val intent = Intent(this, dimen_responsive::class.java)
+            startActivity(intent)
+        }
     }
 }
