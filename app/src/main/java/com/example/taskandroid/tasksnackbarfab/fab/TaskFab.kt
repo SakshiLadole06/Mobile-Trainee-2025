fd18be2d9eb6a01d36d5b0a489496ecfaa7fa3f8
SakshiLadole06 <ladolesakshi@gmail.com>
2025-02-13 11:43:15 +0530

    One Feature Add

--- app/src/main/java/com/example/taskandroid/tasksnackbarfab/fab/TaskFab.kt
+++ app/src/main/java/com/example/taskandroid/tasksnackbarfab/fab/TaskFab.kt
@@ -1,6 +1,7 @@
 package com.example.taskandroid.tasksnackbarfab.fab
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.taskandroid.R
@@ -8,11 +9,22 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TaskFab : AppCompatActivity() {
     private lateinit var fabBtn: FloatingActionButton
+    private lateinit var fabBtnUp: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_fab)
         fabBtn=findViewById(R.id.fabBtn)
         fabBtn.setOnClickListener{
+            fabBtnUp.visibility=View.VISIBLE
+//            Toast.makeText(this,"Floating Action Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        
+
+        //For Up button
+        fabBtnUp=findViewById(R.id.fabBtnUp)
+        fabBtnUp.visibility= View.INVISIBLE
+        fabBtnUp.setOnClickListener{
             Toast.makeText(this,"Floating Action Button Clicked", Toast.LENGTH_SHORT).show()
         }
     }
